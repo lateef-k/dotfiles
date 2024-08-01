3f463025452eba978a3325dd1d172035b12f4d99
lateef <dev@lateefk.com>
2024-08-01 15:26:07 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -20,8 +20,11 @@
   };
 
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
+    substituters = [ 
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org" 
+    ];
+    trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
