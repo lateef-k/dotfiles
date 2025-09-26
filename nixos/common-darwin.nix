5ea46b8a2229d1c42ec7b73d983c232d1fb424b5
lateef <dev@lateefk.com>
2025-09-27 00:54:16 +0300

    simplify flake, disable nix daemon for macos (will use detsys nix instead)

--- nixos/common-darwin.nix
+++ nixos/common-darwin.nix
@@ -1,42 +1,9 @@
 
 
 { inputs, lib, config, pkgs, ... }: {
-  nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
+  nix.enable = false;
 
-    settings = {
-      flake-registry = "";
-      nix-path = config.nix.nixPath;
-      trusted-users = [ "ludvi" ];
-      substituters = [
-        "https://nix-community.cachix.org?priority=20"
-        "https://cache.nixos.org?priority=30"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      ];
-      keep-outputs = true;
-      keep-derivations = true;
-      experimental-features = "nix-command flakes";
-    };
-
-    gc = {
-      automatic = true;
-      interval = {
-        Weekday = 0;
-        Hour = 0;
-        Minute = 0;
-      };
-      options = "--delete-older-than 30d";
-    };
-    channel.enable = false;
-    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    optimise.automatic = true;
-  };
-
-  imports = [ inputs.nix-homebrew.darwinModules.nix-homebrew ];
+  imports = [ ];
 
   nixpkgs = {
     config = {
